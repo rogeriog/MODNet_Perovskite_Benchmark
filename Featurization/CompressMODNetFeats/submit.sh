2ecbb64a95122eb814afd4781de64d33dba2729c
Rogerio Almeida Gouvea (rogerio.almeida@uclouvain.be) <rgouvea@mbackf2.cism.ucl.ac.be>
2022-12-13 00:31:35 +0100

    Encoder first results and MEGNetFeats

---
+++ Featurization/CompressMODNetFeats/submit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=AutoEncoder_MODNetPerovsk
+#SBATCH --time=2-00:00:00
+#SBATCH --output=log.txt
+#SBATCH --nodes=1
+#SBATCH --mem-per-cpu=40000
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+## #SBATCH --nodelist=mb-rom[101,102,103,104]
+source ~/.bashrc
+module load CUDA cuDNN/8.0.4.30-CUDA-11.1.1 
+# TensorFlow/2.5.0-fosscuda-2020b
+#export XLA_FLAGS="--xla_gpu_cuda_data_dir=/home/ucl/modl/rgouvea/anaconda3/envs/env_megnetgpu/lib/"
+CUDA_DIR=/home/ucl/modl/rgouvea/anaconda3/envs/env_modnetmod/lib/
+export XLA_FLAGS="--xla_gpu_cuda_data_dir=/home/ucl/modl/rgouvea/anaconda3/envs/env_modnetmod/lib/" 
+# /home/ucl/modl/rgouvea/xla/nvvm/libdevice"
+
+conda activate env_modnetmod
+echo "start"
+date
+python3 -u AutoEncoderMODNetFeats.py >> log.txt
+date
+echo "done"
+
