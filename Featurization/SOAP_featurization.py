1123ccf8a7c5dda9854453637758d4dacc2c2866
Rogerio Almeida Gouvea (rogerio.almeida@uclouvain.be) <rgouvea@mbackf2.cism.ucl.ac.be>
2022-12-12 16:12:29 +0100

    SOAP featurization updated

--- Featurization/SOAP_featurization.py
+++ Featurization/SOAP_featurization.py
@@ -7,19 +7,19 @@ from ase.data import atomic_numbers
 import os
 import pandas as pd
 
-#from modnet.preprocessing import MODData
-#data=MODData.load('../DATAFILES/matbench_perovskites_moddata.pkl.gz')
-#structures=data.df_structure['structure']
-#try:
-structures_ase=pickle.load(open("ase_structures.pkl","rb"))
-#except:
-#    structures_ase=list(map(AseAtomsAdaptor.get_atoms,structures))
-#    pickle.dump(structures_ase,open("ase_structures.pkl","wb"))
+from modnet.preprocessing import MODData
+data=MODData.load('../DATAFILES/matbench_perovskites_moddata.pkl.gz')
+structures=data.df_structure['structure']
+try:
+    structures_ase=pickle.load(open("ase_structures.pkl","rb"))
+except:
+    structures_ase=list(map(AseAtomsAdaptor.get_atoms,structures))
+    pickle.dump(structures_ase,open("ase_structures.pkl","wb"))
     
 ## declaring the SOAP featurizer
 species=list(atomic_numbers.keys())[1:] ## all chemical species
-nmax=8
-lmax=6
+nmax=3 #8
+lmax=3 #6
 rcut=5
 average_soap = SOAP(species=species,
 rcut=rcut, nmax=nmax, lmax=lmax,
@@ -28,11 +28,21 @@ rcut=rcut, nmax=nmax, lmax=lmax,
     periodic=True,
     sparse=False
 )
-ncpus=os.cpu_count()
+print(f"Total number of features: {average_soap.get_number_of_features()}")
+ncpus=12 # os.cpu_count()
 ## this is very memory intensive has to be splitted
-slices=[None]+list(range(25,len(structures_ase),25))+[None]
+slices=[None]+list(range(200,len(structures_ase),200))+[None]
+
 for i, slice1 in list(enumerate(slices))[:-1]:
-    results = average_soap.create(structures_ase[slice1:slices[i+1]], n_jobs=ncpus)
+    #if i < 245 :
+    #    continue
+    soap_results = average_soap.create(structures_ase[slice1:slices[i+1]], n_jobs=ncpus)
+    results=[]
+    for j in range(len(soap_results)):
+        ## SOAP data is too large and sparse, its better to work with values and index
+        results.append([soap_results[j][soap_results[j].nonzero()[0]],
+                         soap_results[j].nonzero()[0]])
     pickle.dump(results, open(f"SOAP_perovsk_featurized_{i}.pkl","wb"))
+    print(f"Processed SOAP_perovsk_featurized_{i}.pkl, total of {len(slices)-2}")
+    del soap_results
     del results
-    print(f"{i} out of {len(slices)-2} subsets to complete SOAP featurization")
